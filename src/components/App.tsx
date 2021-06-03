d32eb217d2eff758c2bb6ed6aa92dabaa5eea796
Sebastian Borg <Sebastian.borg99@hotmail.se>
2021-06-03 13:46:40 +0200

    layout start

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import './App.css';
+/* import Button from '@material-ui/core/Button'; */
+import Layout from './layout'
 
-function App() {
+export default function App() {
+  
+  return <Layout/>
+  
+}
+
+
+
+/* function App() {
   return 
 }
+ */
