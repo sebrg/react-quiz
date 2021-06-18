d25f931800a2f5aeada73dfaa2d4b375a9f3586e
Sebastian Borg <Sebastian.borg99@hotmail.se>
2021-06-18 14:43:32 +0200

    daw

--- src/components/MainView/loginPage.tsx
+++ src/components/MainView/loginPage.tsx
@@ -13,8 +13,8 @@ export default class User extends React.Component {
  
   
     State = {
-        user: '',
-        score: '',
+        'user': String,
+        'score': Number,
     }
     
     
